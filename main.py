17f5f172cdc735043f85f2fc28728acfe80151e1
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-07-23 22:54:43 +0800

    test push

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 from telegram.ext import Updater
 
-updater = Updater(token='TOKEN', use_context=True)
+updater = Updater(token='1302957992:AAGHAAcJgNBkbd6pSsUbLW85dpxSHBHRkrA', use_context=True)
 
 dispatcher = updater.dispatcher
 
@@ -15,8 +15,14 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+def custom(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Custom text")
+
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
+custom_handler = CommandHandler('custom', custom)
+
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(custom_handler)
 
 updater.start_polling()
