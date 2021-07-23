8653e9bcd784a9c583d840a6e22afc7cf7b4046c
Aqlan Nor Azman <aqlanhadi@gmail.com>
2021-07-23 22:35:00 +0800

    base code

---
+++ main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from dotenv.main import dotenv_values
+load_dotenv()
+
+from telegram.ext import Updater
+
+updater = Updater(token='TOKEN', use_context=True)
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
